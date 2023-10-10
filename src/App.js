af50e9d2d1cb84030e09d60cd91f37846cf3bdcf
fadhel <fadhelalqaidoom@icloud.com>
2023-10-10 18:15:48 +0300

    Finish the converter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+
 function App() {
+  const [value, setValue] = useState(0);
+  const [convert, setConvert] = useState(0);
+  const [from, setFrom] = useState('USD');
+  const [to, setTo] = useState('EUR');
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const controller = new AbortController();
+    const fetchData = async () => {
+      try {
+        setError('');
+        setConvert(0);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${Number(
+            value
+          )}&from=${from}&to=${to}`,
+          { signal: controller.signal }
+        );
+        if (!res.ok) {
+          throw new Error('Something went wrong!');
+        }
+        const data = await res.json();
+        setConvert(data.rates[to]);
+      } catch (err) {
+        if (err.name !== 'AbortError') setError(err.message);
+      }
+    };
+    fetchData();
+    return () => {
+      controller.abort();
+    };
+  }, [value, from, to]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <select
+        value={from}
+        onChange={(e) => {
+          if (e.target.value === to) {
+            const tempFrom = e.target.value;
+            setTo(from);
+            setFrom(tempFrom);
+          } else {
+            setFrom(e.target.value);
+          }
+        }}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={to}
+        onChange={(e) => {
+          if (e.target.value === from) {
+            const tempTo = e.target.value;
+            setFrom(to);
+            setTo(tempTo);
+          } else {
+            setTo(e.target.value);
+          }
+        }}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{value ? `${convert} ${to}` : 'OUTPUT'}</p>
     </div>
   );
 }
